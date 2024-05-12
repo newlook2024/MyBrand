59f9415a349e9b1aca73cbb40207c70c37f04dde
newlook2024 <abdulvahob2022@gmail.com>
2024-05-12 11:13:54 +0500

    section end

--- js/app.js
+++ js/app.js
@@ -87,63 +87,75 @@ cardData.forEach((item, index) => {
     <img src="${item.Image}" alt="brend image">
     <span class="flex brend__card__text">Load ${item.id}</span>
     `
-    if(item.id > 6){
+    if (item.id > 6) {
         a.classList.add('r__hidden')
     }
 
     cardBox.appendChild(a)
 })
-//  BEST SECTION STYLES
+//  BEST SECTION
 
 let bestCardBox = document.getElementById('best-card-box')
 let bestData = [
     {
         img: "./images/home/best/1.png",
         text: "Daradrpipara’s Content -  Bellazon ",
+        id: 1
     },
     {
         img: "./images/home/best/2.png",
         text: "Daradrpipara’s Content -  Bellazon ",
+        id: 2
     },
     {
         img: "./images/home/best/3.png",
         text: "Daradrpipara’s Content -  Bellazon ",
+        id: 3
     },
     {
         img: "./images/home/best/4.png",
         text: "Daradrpipara’s Content -  Bellazon ",
+        id: 4
     },
     {
         img: "./images/home/best/5.png",
         text: "Daradrpipara’s Content -  Bellazon ",
+        id: 5
     },
     {
         img: "./images/home/best/6.png",
         text: "Daradrpipara’s Content -  Bellazon ",
+        id: 6
     },
     {
         img: "./images/home/best/7.png",
         text: "Daradrpipara’s Content -  Bellazon ",
+        id: 7
     },
     {
         img: "./images/home/best/8.png",
         text: "Daradrpipara’s Content -  Bellazon ",
+        id: 8
     },
     {
         img: "./images/home/best/9.png",
         text: "Daradrpipara’s Content -  Bellazon ",
+        id: 9
     },
     {
         img: "./images/home/best/10.png",
         text: "Daradrpipara’s Content -  Bellazon ",
+        id: 10
     },
     {
         img: "./images/home/best/11.png",
         text: "Daradrpipara’s Content -  Bellazon ",
+        id: 11
     },
     {
         img: "./images/home/best/12.png",
         text: "Daradrpipara’s Content -  Bellazon ",
+        id: 12
     },
 ]
 
@@ -151,5 +163,69 @@ bestData.forEach((item, index) => {
     let a = document.createElement('div')
     a.classList.add('best__card')
     a.innerHTML = `<img src="${item.img}" alt="rasm"> <p class="best__card__text">${item.text}</p>`
+    if (item.id > 6) {
+        a.classList.add('r__hidden')
+    }
     bestCardBox.appendChild(a)
+})
+
+// PARTNESR SECTION
+
+let partnersBox = document.getElementById('partners-box')
+let partners_icon = [
+    {
+        img: "./images/home/partners/1.png",
+        id: 1,
+    },
+    {
+        img: "./images/home/partners/2.png",
+        id: 2,
+    },
+    {
+        img: "./images/home/partners/3.png",
+        id: 3,
+    },
+    {
+        img: "./images/home/partners/4.png",
+        id: 4,
+    },
+    {
+        img: "./images/home/partners/5.png",
+        id: 5,
+    },
+    {
+        img: "./images/home/partners/6.png",
+        id: 6,
+    },
+    {
+        img: "./images/home/partners/7.png",
+        id: 7,
+    },
+    {
+        img: "./images/home/partners/8.png",
+        id: 8,
+    },
+    {
+        img: "./images/home/partners/9.png",
+        id: 9,
+    },
+    {
+        img: "./images/home/partners/10.png",
+        id: 10,
+    },
+    {
+        img: "./images/home/partners/11.png",
+        id: 11,
+    },
+    {
+        img: "./images/home/partners/12.png",
+        id: 12,
+    },
+]
+
+partners_icon.forEach((item, index) => {
+    let a = document.createElement('div')
+    a.classList.add('partners__icon')
+    a.innerHTML = `<img src="${item.img}" alt="icon">`
+    partnersBox.appendChild(a)
 })
