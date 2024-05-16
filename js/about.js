e7fc434327797ce81a79056be22dcda5cee6ee93
newlook2024 <abdulvahob2022@gmail.com>
2024-05-16 21:49:30 +0500

    shop page ended

--- js/about.js
+++ js/about.js
@@ -33,3 +33,67 @@ heroImg.forEach((item, index) => {
         mainImg.src = item.src
     })
 })
+
+
+let offerCardBox = document.getElementById('offerCardBox')
+let offerCard = [
+    {
+        img: "../images/home/best/1.png",
+        text: "Daradrpipara’s Content -  Bellazon ",
+        id: 1
+    },
+    {
+        img: "../images/home/best/2.png",
+        text: "Daradrpipara’s Content -  Bellazon ",
+        id: 2
+    },
+    {
+        img: "../images/home/best/3.png",
+        text: "Daradrpipara’s Content -  Bellazon ",
+        id: 3
+    },
+    {
+        img: "../images/home/best/4.png",
+        text: "Daradrpipara’s Content -  Bellazon ",
+        id: 4
+    },
+    {
+        img: "../images/home/best/5.png",
+        text: "Daradrpipara’s Content -  Bellazon ",
+        id: 5
+    },
+    {
+        img: "../images/home/best/6.png",
+        text: "Daradrpipara’s Content -  Bellazon ",
+        id: 6
+    },
+]
+
+offerCard.forEach((item, index) => {
+    let a = document.createElement('div')
+    a.classList.add('best__card')
+    a.innerHTML = `<img src="${item.img}" alt="rasm"> <p class="best__card__text">${item.text}</p>`
+    if (item.id > 4) {
+        a.classList.add('r__hidden')
+    }
+    offerCardBox.appendChild(a)
+})
+
+let emailTester = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/
+let footerInp = document.querySelector('#footer-inp')
+let form = document.getElementById('form')
+let submit = document.getElementById('submit')
+
+footerInp.addEventListener('input', (e) => {
+    if (!footerInp.value.match(emailTester)) {
+        submit.classList.remove('trueBtn')
+    } else {
+        submit.classList.add('trueBtn')
+    }
+})
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    footerInp.value = null
+    submit.classList.remove('trueBtn')
+})
